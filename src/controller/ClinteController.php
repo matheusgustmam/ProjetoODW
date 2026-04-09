868ec35e976133eee780f01a3d7e6eb59d8bfc01
matheus gustmam <matheusgustmam@gmail.com>
2026-04-09 20:52:37 -0300

    atulizacao com algumas views

---
+++ src/controller/ClinteController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace controller;
+
+
+class ClienteController{
+    public function index(){
+        require __DIR__ . '/../../view/pagina-inicial.php';
+
+    }
+}
---
+++ src/view/pagina-inicial.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Projeto Exemplo | Home</title>
+</head>
+<body>
+    <h1>Página inicial</h1>
+</body>
+</html>
